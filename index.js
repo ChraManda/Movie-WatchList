875c9500460320f018707bc7abaeca09df7966bf
Chradoreng M Sangma <125442119+ChraManda@users.noreply.github.com>
2025-11-11 17:55:01 +0530

    finished homepage design
    design with the dummy data completed for homepage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// fetch ("https://www.omdbapi.com/?apikey=45445705&s=batman")
+// fetch ("https://www.omdbapi.com/?apikey=45445705&t=superman")
 //     .then(res => res.json())
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err));
