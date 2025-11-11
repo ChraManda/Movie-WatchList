1b266d57c76aff536f6eb647df2d3df8852f9c7c
Chradoreng M Sangma <125442119+ChraManda@users.noreply.github.com>
2025-11-11 21:27:52 +0530

    feat: fetch and display movies from API

--- index.js
+++ index.js
@@ -4,3 +4,52 @@
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err));
 
+const moviesEl = document.getElementById("movies")
+
+const searchMovies = []
+
+
+const fetchMovies = async movie => {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=45445705&s=${movie}`)
+        const data = await res.json()
+        
+        if (data.Response === "True") {
+            for (let item of data.Search) {
+                const detailSearch = await fetch(`https://www.omdbapi.com/?apikey=45445705&i=${item.imdbID}`)
+                const detailData = await detailSearch.json()
+                searchMovies.push(detailData)
+            }
+        } else {
+            console.log("Movie not found")
+        }
+}
+
+const renderMovies = async () => {
+
+    await fetchMovies('batman')
+
+    const movies = searchMovies.map(movie => 
+        `<div id="movies-container" class="movies-container">
+                        <img class="movie-poster" src="${movie.Poster}" alt="">
+                        <div class="movie-desc">
+                            <div class="movie-header">
+                                <h3 class="movie-title">${movie.Title}</h3>
+                                <p class="movie-ratings">⭐ ${movie.imdbRating}/10</p>
+                            </div>
+                            <div class="movie-info">
+                                <p>${movie.Runtime}</p>
+                                <p>${movie.Genre}</p>
+                                <button id='${movie.imdbID}' class="watchlist-button">
+                                    <i class="fa-solid fa-plus"></i> Watchlist
+                                </button>
+                            </div>
+                                <p class="movie-summary">${movie.Plot}</p>
+                        </div>
+                    </div>
+                    `
+    ).join('')
+
+    moviesEl.innerHTML = movies
+}
+    
+renderMovies()
